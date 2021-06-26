d0ee43113f7be25cad2c049548bfa895d21c2f3c
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-06-26 12:52:40 +0530

    --add: Created a navbar for youtube

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./styles/style.css'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem'
+      },
+      fontFamily: {
+        'Roboto' : ['Roboto','Arial', 'sans-serif'],
+      }
+    },
   },
   variants: {
     extend: {},
